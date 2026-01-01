7225a92d5b4420e80f98ee18760acda703ee3114
iamnityatiwari <149710274+iamnityatiwari@users.noreply.github.com>
2026-01-01 18:16:11 +0530

    Add task statistics route and reorganize routes

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const router = express.Router();
-const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController');
+const { getTasks, createTask, updateTask, deleteTask, getTaskStats } = require('../controllers/taskController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.route('/').get(protect, getTasks).post(protect, createTask);
-router.route('/:id').put(protect, updateTask).delete(protect, deleteTask);
+const router = express.Router();
+
+router.route('/')
+    .get(protect, getTasks)
+    .post(protect, createTask);
+
+router.get('/stats', protect, getTaskStats);
+
+router.route('/:id')
+    .put(protect, updateTask)
+    .delete(protect, deleteTask);
 
 module.exports = router;
